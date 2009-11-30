3a643c83ae8fa8795fc65553a5250dcbaf544c2a
taelor <taelor@co-hack.com>
2009-11-30 16:20:25 -0600

    empty pass

--- cookbooks/users/attributes/default.rb
+++ cookbooks/users/attributes/default.rb
@@ -12,10 +12,10 @@ users Mash.new unless attribute?("users")
  
 # passwords must be in shadow password format with a salt. To generate: openssl passwd -1
  
-users[:taelor] = {:password => "hackme", :comment => "Taylor Redden", :uid => 4001, :group => :admin}
-users[:penguincoder] = {:password => "hackme", :comment => "Andrew Coleman", :uid => 4002, :group => :admin}
-users[:rledge21] = {:password => "hackme", :comment => "Ralph Edge", :uid => 4003, :group => :admin}
-users[:cohack] = {:password => "hackme", :comment => "Co-Hack", :uid => 5000, :group => :admin} 
+users[:taelor] = {:password => "", :comment => "Taylor Redden", :uid => 4001, :group => :admin}
+users[:penguincoder] = {:password => "", :comment => "Andrew Coleman", :uid => 4002, :group => :admin}
+users[:rledge21] = {:password => "", :comment => "Ralph Edge", :uid => 4003, :group => :admin}
+users[:cohack] = {:password => "", :comment => "Co-Hack", :uid => 5000, :group => :admin} 
  
 #ssh_keys[:jose] = "ssh-dss keydata"
 #ssh_keys[:francisco] = "ssh-rsa keydata"
