01b17039fa155e3fc285e537a1519f423e2eeb23
taelor <taelor@co-hack.com>
2009-11-30 14:18:59 -0600

    changes to config file

--- config/solo.rb
+++ config/solo.rb
@@ -3,9 +3,9 @@
 #
 
 cookbook_path File.join(File.dirname(__FILE__), '..', "cookbooks")
-
 log_level          :info
 log_location       STDOUT
-file_cache_path    "/srv/chef/cookbooks"
+file_store_path File.join(File.dirname(__FILE__), '..')
+file_cache_path File.join(File.dirname(__FILE__), '..')
 ssl_verify_mode    :verify_none
 Chef::Log::Formatter.show_time = false
