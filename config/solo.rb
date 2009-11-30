902bbc9c94776516c344ff71cbafd69a90c2c84e
taelor <taelor@co-hack.com>
2009-11-30 14:17:26 -0600

    gotta know where the files are...

--- config/solo.rb
+++ config/solo.rb
@@ -2,6 +2,8 @@
 # Chef Solo Config File
 #
 
+cookbook_path File.join(File.dirname(__FILE__), '..', "cookbooks")
+
 log_level          :info
 log_location       STDOUT
 file_cache_path    "/srv/chef/cookbooks"
--- cookbooks/main/metadata.rb
+++ cookbooks/main/metadata.rb
@@ -4,5 +4,3 @@ license          "Apache 2.0"
 description      "Installs/Configures main"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
-
-depends 'main'
