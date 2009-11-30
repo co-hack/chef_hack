d478906fbb62693326d6ba7a1da22aa95201a289
taelor <taelor@co-hack.com>
2009-11-30 14:47:57 -0600

    added users recipe, for real this time

--- cookbooks/users/recipes/default.rb
+++ cookbooks/users/recipes/default.rb
@@ -17,23 +17,16 @@
 # limitations under the License.
 #
 
-#users Mash.new unless attribute?("users")
-#groups Mash.new unless attribute?("groups")
-#ssh_keys Mash.new unless attribute?("ssh_keys")
-#roles Mash.new unless attribute?("roles")
- 
-#groups[:app] = {:gid => 5000}
-#groups[:site] = {:gid => 6000}
-#groups[:admin] = {:gid => 7000}
- 
-#roles[:chef] = {:groups => [:admin], :sudo_groups => [:admin]}
-#roles[:app] = {:groups => [:admin, :app], :sudo_groups => [:admin]}
- 
-# passwords must be in shadow password format with a salt. To generate: openssl passwd -1
- 
-users[:taelor] = {:password => "shadowpass", :comment => "Taylor Redden", :uid => 4001, :group => :admin}
-users[:pengiuncoder] = {:password => "shadowpass", :comment => "Andrew Coleman", :uid => 4002, :group => :admin}
-users[:rledge] = {:password => "shadowpass", :comment => "Ralph Edge", :uid => 4003, :group => :admin}
- 
-#ssh_keys[:jose] = "ssh-dss keydata"
-#ssh_keys[:francisco] = "ssh-rsa keydata"
+['taelor', 'penguincoder', 'rledge'].each do |username|
+  config = node[:users][username]
+  user username do
+    comment config[:comment]
+    uid config[:uid]
+    gid config[:group]
+    home "/home/#{username}"
+    shell "/bin/bash"
+    password config[:password]
+    supports :manage_home => true
+    action [:create, :manage]
+  end
+end
