8ed78215d890c032f0b0fe62da65a7df90804588
taelor <taelor@co-hack.com>
2009-11-30 22:18:40 -0600

    nginx and god recipes

--- cookbooks/god/recipes/default.rb
+++ cookbooks/god/recipes/default.rb
@@ -1,25 +1,41 @@
 #
-# Cookbook Name:: main
+# Cookbook Name:: god
 # Recipe:: default
 #
-# Copyright 2009, Example Com
+# Copyright 2009, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include_recipe 'ruby-shadow'
-include_recipe 'users'
-include_recipe 'mysql'
-include_recipe 'gems'
-include_recipe 'nginx'
-include_recipe 'unicorn'
-include_recipe 'god'
+
+#include_recipe "ruby"
+#include_recipe "runit"
+
+gem_package "god" do
+  action :install
+end
+
+directory "/etc/god/conf.d" do
+  recursive true
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+template "/etc/god/master.god" do
+  source "master.god.erb"
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+#runit_service "god"
