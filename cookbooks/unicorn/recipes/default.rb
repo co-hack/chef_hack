e93ac7366882baa64981d729825ccab6354de538
taelor <taelor@co-hack.com>
2009-11-30 22:33:23 -0600

    more unicorn

--- cookbooks/unicorn/recipes/default.rb
+++ cookbooks/unicorn/recipes/default.rb
@@ -17,4 +17,18 @@
 # limitations under the License.
 #
 
-package "unicorn"
+gem_package "unicorn" do
+  version node[:unicorn][:version]
+end
+ 
+directory "/etc/unicorn" do
+  owner "app"
+  group "app"
+  mode 0755
+end
+ 
+directory "/tmp/unicorn" do
+  owner "app"
+  group "app"
+  mode 0755
+end
